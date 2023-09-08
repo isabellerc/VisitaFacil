937b8dee82a47691e91fcded36ddc9429117bd73
isabellerc <isabellereco1@gmail.com>
2023-09-08 14:37:56 -0300

    Atualizações do projeto

---
+++ VisitaFacil.WebApp/Controllers/AutenticadorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VisitaFacil.WebApp.Models;
+
+namespace VisitaFacil.WebApp.Controllers
+{
+    public class AutenticadorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Autenticar(UsuarioViewModel entidade)
+        {
+            if (entidade.Autenticado())
+                await Response.WriteAsync("Resultado Positivo");
+            else
+                await Response.WriteAsync("Resultado Negativo");
+            return null;
+        }
+    }
+}
---
+++ VisitaFacil.WebApp/Models/UsuarioViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace VisitaFacil.WebApp.Models
+{
+    public class UsuarioViewModel
+    {
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+        public bool ManterConectado { get; set; }
+        public bool Autenticado()
+        {
+            if (Usuario == "renato" && Senha == "123")
+                return true;
+            else
+                return false;
+        }
+    }
+}
