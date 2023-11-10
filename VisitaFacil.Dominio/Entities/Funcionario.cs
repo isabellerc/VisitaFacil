8564ba28f5c1f9c6ea0811042ca9643961a0ca5a
Vitor Hugo Resende <vitorhr.vasconcelos@gmail.com>
2023-11-09 22:24:35 -0300

    .

--- VisitaFacil.Dominio/Entities/Funcionario.cs
+++ VisitaFacil.Dominio/Entities/Funcionario.cs
@@ -9,7 +9,17 @@ namespace VisitaFacil.Dominio.Entities
 {
     public class Funcionario : DadosPessoais
     {
-        
-        //public int IDFUNCIONARIO { get; set; }
+        [Key]
+        public int idFuncionario { get; set; }
+
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime dataNascimento { get; set; }
+
+        public string Endereco { get; set; }
+        public string Telefone1 { get; set; }
+        public string Telefone2 { get; set; }
+        public string Email { get; set; }
+        public bool Ativo { get; set; }
     }
 }
--- VisitaFacil.WebApp/Controllers/FuncionarioController.cs
+++ VisitaFacil.WebApp/Controllers/FuncionarioController.cs
@@ -1,6 +1,7 @@
 ﻿//using Microsoft.AspNetCore.Mvc;
 //using VisitaFacil.Dados;
 //using VisitaFacil.Dominio.Entities;
+//using VisitaFacil.WebApp.Models;
 
 //namespace VisitaFacil.WebApp.Controllers
 //{
@@ -23,12 +24,44 @@
 //        }
 
 //        [HttpPost]
-//        public IActionResult InserirConfirmar(Funcionario ent)
+//        public IActionResult Post(Funcionario ent)
 //        {
 //            db.Funcionario.Add(ent);
 //            db.SaveChanges();
+//            return RedirectToAction("Login", "Home");
+
+//        }
+//        [HttpPost]
+//        public IActionResult Login(Funcionario ent)
+//        {
+//            var criarUsuarioLogin = new UsuarioViewModel();
+//            criarUsuarioLogin.Usuario = ent.Email;
+//            criarUsuarioLogin.Senha = ent.Cpf;
+//            if (criarUsuarioLogin.Autenticado())
+//            {
+//                return RedirectToAction("Index", "Funcionario");
+//            }
+//            return RedirectToAction("Login", "Home");
+//        }
+
+//        public IActionResult Excluir(int ID)
+//        {
+//            var objeto = db
+//                .Funcionario
+//                .First(f => f.ID == ID);
+
+//            db.Funcionario.Remove(objeto);
+//            db.SaveChanges();
+
 //            return RedirectToAction("Index");
 //        }
+//        //[HttpPost]
+//        //public IActionResult InserirConfirmar(Funcionario ent)
+//        //{
+//        //    db.Funcionario.Add(ent);
+//        //    db.SaveChanges();
+//        //    return RedirectToAction("Index");
+//        //}
 
 //        //public IActionResult Excluir(int funcionarioId)
 //        //{
